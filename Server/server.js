b9e77c3fb365cba502852fd5bb68f4bae1275bb1
Mabroor007 <amabroor007@gmail.com>
2020-10-12 09:48:34 -0700

    added MongoDb Database

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const GraphQl = require("./Graphql/gqlServer");
+const mongoose = require("mongoose");
 
 // App initialization
 const app = express();
 
-// Middleware
+// Database Connection
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/shopit",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("Database Connected 🚀 ...")
+);
+
+// Middleware for GraphQl api
 GraphQl.applyMiddleware({ app });
 
 app.listen(3000, () => {
-  console.log("Server Up and Running on port 3000...");
+  console.log("Server Up and Running 🚀 on port 3000...");
 });
